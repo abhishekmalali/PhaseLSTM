91ffda8d486c9ace3463237ff8f4ff3fef264b11
Abhishek Malali <abhishekmalali@gmail.com>
2017-01-11 11:01:34 -0300

    Adding clasify with PLSTM code

---
+++ classify/classify_model.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+import numpy as np
+from tabulate import tabulate
+from tensorflow.python.ops.rnn_cell import LSTMCell, GRUCell
+from PhasedLSTMCell import PhasedLSTMCell, multiPLSTM
+from data_generation import create_batch_dataset
+from tqdm import tqdm
+flags = tf.flags
+flags.DEFINE_string("unit", "PLSTM", "Can be PSLTM, LSTM, GRU")
+flags.DEFINE_integer("n_hidden", 5, "hidden units in the recurrent layer")
+flags.DEFINE_integer("n_epochs", 5, "number of epochs")
+flags.DEFINE_integer("batch_size", 5, "batch size")
+flags.DEFINE_integer("b_per_epoch", 2, "batches per epoch")
+flags.DEFINE_integer("n_layers", 1, "hidden units in the recurrent layer")
+flags.DEFINE_float("exp_init", 3., "Value for initialization of Tau")
+flags.DEFINE_string('train_ckpt', 'ckpts/trial/model_ini.ckpt', 'Train checkpoint file')
+flags.DEFINE_string('train_logs', 'tmp/trail/', 'Log directory')
+FLAGS = flags.FLAGS
+
+n_input = 1
+n_out = 2
+
+def RNN(_X, _weights, _biases, lens):
+    if FLAGS.unit == "PLSTM":
+        cell = PhasedLSTMCell(FLAGS.n_hidden, use_peepholes=True, state_is_tuple=True)
+    elif FLAGS.unit == "GRU":
+        cell = GRUCell(FLAGS.n_hidden)
+    elif FLAGS.unit == "LSTM":
+        cell = LSTMCell(FLAGS.n_hidden, use_peepholes=True, state_is_tuple=True)
+    else:
+        raise ValueError("Unit '{}' not implemented.".format(FLAGS.unit))
+    initial_states = [tf.nn.rnn_cell.LSTMStateTuple(tf.zeros([FLAGS.batch_size, FLAGS.n_hidden], tf.float32), tf.zeros([FLAGS.batch_size, FLAGS.n_hidden], tf.float32)) for _ in range(FLAGS.n_layers)]
+    outputs, initial_states = multiPLSTM(_X, FLAGS.batch_size, lens, FLAGS.n_layers, FLAGS.n_hidden, n_input, initial_states)
+
+    outputs = tf.slice(outputs, [0, 0, 0], [-1, -1, FLAGS.n_hidden])
+
+    batch_size = tf.shape(outputs)[0]
+    max_len = tf.shape(outputs)[1]
+    out_size = int(outputs.get_shape()[2])
+    index = tf.range(0, batch_size) * max_len + (lens - 1)
+    flat = tf.reshape(outputs, [-1, out_size])
+    relevant = tf.gather(flat, index)
+
+    return tf.nn.bias_add(tf.matmul(relevant, _weights['out']), _biases['out']), initial_states
+
+def build_model():
+    x = tf.placeholder(tf.float32, [None, None, n_input + 1])
+    lens = tf.placeholder(tf.int32, [None])
+    #labels
+    y = tf.placeholder(tf.float32, [None, n_out])
+    # weights from input to hidden
+    weights = {
+        'out': tf.Variable(tf.random_normal([FLAGS.n_hidden, n_out], dtype=tf.float32))
+    }
+
+    biases = {
+        'out': tf.Variable(tf.random_normal([n_out], dtype=tf.float32))
+    }
+
+    # Register weights to be monitored by tensorboard
+    w_out_hist = tf.summary.histogram("weights_out", weights['out'])
+    b_out_hist = tf.summary.histogram("biases_out", biases['out'])
+    print ("Compiling RNN...",)
+    predictions, initial_states = RNN(x, weights, biases, lens)
+    print ("DONE!")
+    # Register initial_states to be monitored by tensorboard
+    initial_states_hist = tf.summary.histogram("initial_states", initial_states[0][0])
+    print ("Compiling cost functions...",)
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(predictions, y))
+    print ("DONE!")
+    cost_summary = tf.summary.scalar("cost", cost)
+    cost_val_summary = tf.summary.scalar("cost_val", cost)
+    optimizer = tf.train.AdamOptimizer().minimize(cost)
+    # evaluation
+    correct_pred = tf.equal(tf.argmax(predictions, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    accuracy_summary = tf.summary.scalar("accuracy", accuracy)
+    accuracy_val_summary = tf.summary.scalar("accuracy_val", accuracy)
+
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
+    columns = ['Epoch', 'train_cost', 'train_acc', 'val_cost', 'val_acc']
+    log_df = pd.DataFrame(data=np.zeros((0,len(columns))), columns=columns)
+    with tf.Session(config=config) as sess:
+        sess.run(init)
+        writer = tf.summary.FileWriter(FLAGS.train_logs, sess.graph)
+        for step in range(FLAGS.n_epochs):
+            train_cost = 0
+            train_acc = 0
+            for i in tqdm(range(FLAGS.b_per_epoch)):
+                batch_xs, batch_ys, leng = create_batch_dataset(FLAGS.batch_size)
+                res = sess.run([optimizer, cost, accuracy, cost_summary, accuracy_summary],
+                               feed_dict={x: batch_xs,
+                                          y: batch_ys,
+                                          lens: leng
+                                          })
+                writer.add_summary(res[3], step * FLAGS.b_per_epoch + i)
+                writer.add_summary(res[4], step * FLAGS.b_per_epoch + i)
+                train_cost += res[1] / FLAGS.b_per_epoch
+                train_acc += res[2] / FLAGS.b_per_epoch
+            print "Epoch "+ str(step+1) +" train_cost: "+str(train_cost)+" train_accuracy: "+str(train_acc)
+            batch_val_xs, batch_val_ys, leng_val = create_batch_dataset(FLAGS.batch_size)
+            loss_test, acc_test, summ_cost, summ_acc = sess.run([cost,
+                                            accuracy, cost_val_summary, accuracy_val_summary],
+                                            feed_dict={x: batch_val_xs,
+                                                       y: batch_val_ys,
+                                                       lens: leng_val})
+            writer.add_summary(summ_cost, step * FLAGS.b_per_epoch + i)
+            writer.add_summary(summ_acc, step * FLAGS.b_per_epoch + i)
+            table = [["Train", train_cost, train_acc],
+                     ["Test", loss_test, acc_test]]
+            headers = ["Epoch={}".format(step), "Cost", "Accuracy"]
+            log_df = log_df.append({'Epoch': step+1,
+                                    'train_cost': train_cost,
+                                    'train_acc': train_acc,
+                                    'val_cost': val_cost,
+                                    'val_acc': val_acc})
+            print (tabulate(table, headers, tablefmt='grid'))
+        saver.save(sess, FLAGS.train_ckpt)
+        log_df.to_csv('log_trial.csv')
+
+def main(argv=None):
+    with tf.device('/cpu:0'):
+        build_model()
+
+if __name__ == '__main__':
+  tf.app.run()
---
+++ classify/data_generation.py
@@ -0,0 +1,78 @@
+import numpy as np
+import timesynth as ts
+
+def generate_gp_data(stop_time, variance, lengthscale ,num_points,
+                     keep_percentage, noise):
+    time_sampler = ts.TimeSampler(stop_time=stop_time)
+    irregular_time_samples = time_sampler.sample_irregular_time(num_points=num_points,
+                                                                keep_percentage=keep_percentage)
+    gp_object = ts.signals.GaussianProcess(variance=variance, lengthscale=lengthscale, kernel='SE')
+    white_noise = ts.noise.GaussianNoise(std=noise)
+    timeseries = ts.TimeSeries(gp_object, noise_generator=white_noise)
+    samples, signals, errors = timeseries.sample(irregular_time_samples)
+    # Reshaping to concatenate
+    samples = np.reshape(samples, (len(samples), 1))
+    irregular_time_samples = np.reshape(irregular_time_samples, (len(irregular_time_samples), 1))
+    return np.concatenate((samples, irregular_time_samples), axis=1)
+
+def generate_sin_data(stop_time, amplitude, freq ,num_points, keep_percentage, noise):
+    time_sampler = ts.TimeSampler(stop_time=stop_time)
+    irregular_time_samples = time_sampler.sample_irregular_time(num_points=num_points,
+                                                                keep_percentage=keep_percentage)
+    sinusoidal_object = ts.signals.Sinusoidal(frequency=freq, amplitude=amplitude)
+    white_noise = ts.noise.GaussianNoise(std=noise)
+    timeseries = ts.TimeSeries(sinusoidal_object, noise_generator=white_noise)
+    samples, signals, errors = timeseries.sample(irregular_time_samples)
+    # Reshaping to concatenate
+    samples = np.reshape(samples, (len(samples), 1))
+    irregular_time_samples = np.reshape(irregular_time_samples, (len(irregular_time_samples), 1))
+    return np.concatenate((samples, irregular_time_samples), axis=1)
+
+
+def create_batch_dataset(batch_size):
+    # Deciding the number of datapoints for each class
+    class_dist_array = np.random.choice([0,1], replace=True, size=batch_size)
+    # Number of points between 1000 and 2000
+    num_points = np.random.randint(1000, 2000, size= batch_size)
+    # Sparsity between 25% and 75%
+    kp = np.random.choice(np.linspace(25, 75, num=11), size= batch_size)
+    # Setting stop time
+    stop_time_values = np.random.uniform(low=10., high=50., size = batch_size)
+    # Calculating the maximum length
+    max_len = np.max(0.01*np.multiply(num_points, kp))
+    # Creating an empty matrix batch_size x max_len x input_dimension
+    n_input = 2
+    n_output = 2
+    X = np.zeros((batch_size, max_len, n_input))
+    Y = np.zeros((batch_size, n_output))
+
+    # Sampling parameters for the sinusoidal datapoints
+    amplitude_values = np.random.uniform(low=0.5, high=2., size= batch_size)
+    freq_values = np.random.uniform(low=0.2, high=2., size= batch_size)
+    noise_values = np.random.uniform(low=0, high=0.5, size= batch_size)
+
+    # Sampling parameters for the gaussian datapoints
+    lengthscale_values = np.random.uniform(low=0.2, high=1., size= batch_size)
+    variance_values = np.random.uniform(low=0.1, high=1., size= batch_size)
+    # Populating the master data array
+    length_array = np.zeros(batch_size)
+    for i in range(batch_size):
+        if class_dist_array[i] == 0:
+            data = generate_sin_data(stop_time_values[i],
+                                     amplitude_values[i],
+                                     freq_values[i],
+                                     num_points[i],
+                                     kp[i], noise_values[i])
+            X[i, :len(data), :] = data
+            Y[i, 0] = 1
+        else:
+            data = generate_gp_data(stop_time_values[i],
+                                    variance_values[i],
+                                    lengthscale_values[i],
+                                    num_points[i],
+                                    kp[i], noise_values[i])
+            X[i, :len(data), :] = data
+            Y[i, 1] = 1
+        length_array[i] = len(data)
+
+    return X, Y, length_array
